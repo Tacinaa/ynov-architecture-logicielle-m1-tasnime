790207bc5da01b6953ae487718c62dfc68aedc2b
Tacinaa <tasnimeyahiaoui59@gmail.com>
2024-11-19 17:01:46 +0100

    init singleton

---
+++ design-pattern/src/main/java/org/example/creational/singleton/OrderManagementService.java
@@ -0,0 +1,14 @@
+package org.example.creational.singleton;
+
+public class OrderManagementService {
+    private static OrderManagementService instance;
+
+    private OrderManagementService() {}
+
+    public static OrderManagementService getInstance() {
+        if (instance == null) {
+            instance = new OrderManagementService();
+        }
+        return instance;
+    }
+}
