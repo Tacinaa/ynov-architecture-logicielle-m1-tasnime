610825dede0826b300342a8704ce14c0e5902ab9
Tacinaa <tasnimeyahiaoui59@gmail.com>
2024-11-19 11:50:28 +0100

    exo s

--- solid/src/main/java/org/example/o/solution/LoanHandler.java
+++ solid/src/main/java/org/example/o/solution/LoanHandler.java
@@ -1,6 +1,24 @@
 package org.example.o.solution;
 
-public interface LoanHandler {
-    void approveLoan(User user);
+public class LoanHandler {
+
+    private PersonaLoanValidator personaLoanValidator;
+    private GageLoanValidator gageLoanValidator;
+
+    public void approvePersonalLoan(User user){
+        if(personaLoanValidator.isValidUserPersonalLoan(user)){
+            System.out.println("Loan validated");
+        }
+    }
+
+
+    public void approveGageLoan(User user){
+        if(gageLoanValidator.isValidUserGage(user)){
+            System.out.println("Loan validated");
+        }
+    }
+
+
+
 
 }
